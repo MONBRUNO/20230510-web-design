1929d5944a2951e16a9826827c4dad4b114aaec9
MONBRUNO <christin6545@naver.com>
2023-05-10 17:09:52 +0900

    web

--- script/common.js
+++ script/common.js
@@ -1,34 +1,32 @@
 $(function(){
-    //메뉴
+    // 메뉴        
     $('.gnb').hover(
         function(){
             $('.gnb ul').stop().slideDown('fast')
-        }
+        },
         function(){
             $('.gnb ul').stop().slideUp('fast')
         }
-    )
-
-    //슬라이드
-    $('.slide a:first')
-
+    )  
+    
+    // 슬라이드
     setInterval(function(){
-        $('.slide').animate({top:-300},function(){
+        $('.slide').animate({top:-300}, function(){
             $('.slide a:first').appendTo($('.slide'))
-            $('.slide').css('top',0)
+            $('.slide').css('top', 0)
         })
-    }, 3000)
+    },3000)
 
-    //탭
+    // 탭
     $('.tab_title h2').click(function(){
         $('.tab_title h2').removeClass('on')
-        $(this).addClass('on')
+        $(this).addClass('on')           
 
-        $('.tab_contents ul').css('disply','none')
-        $('.tab_contents ul').eq($(this).index()).css('display','block')
+        $('.tab_contents ul').css('display', 'none')
+        $('.tab_contents ul').eq($(this).index()).css('display', 'block')
     })
-    
-    //모달
+
+    // 모달
     $('.notice li:first').click(function(){
         $('#modal').addClass('active')
     })
